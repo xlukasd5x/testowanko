37c1c50d5dfcc675ae5b8daa83fc3e0b97908bd3
mjochab <mjochab@ur.edu.pl>
2019-11-05 21:01:43 +0100

    Update TescikTest.java

--- Tescik/test/tescik/TescikTest.java
+++ Tescik/test/tescik/TescikTest.java
@@ -40,6 +40,7 @@ public class TescikTest {
     @Test
     public void testVersion() {
 	final boolean JAVA_7_OR_EARLIER = System.getProperty("java.specification.version").compareTo("1.8") < 0;
+	System.out.println(System.getProperty("java.specification.version"));
 	assertTrue(JAVA_7_OR_EARLIER);
     }
 
